81505e956858e98203e4a709e9df81e94bc8e435
ryomakawakami <ryomakwkm@gmail.com>
2020-10-16 12:44:20 -0400

    Added wait for pixel color

--- open.py
+++ open.py
@@ -1,4 +1,5 @@
 import pyautogui
+from PIL import ImageGrab
 import time
 
 wifi = (1153, 746)
@@ -8,20 +9,24 @@ connect = (478, 320)
 proceed = (404, 660)
 next = (638, 708)
 next2 = (644, 417)
-password = (411, 397)
+password = (404, 394)
 
-def click(pos, x = 0.1):
+def click(pos, colorPos = None, color = None, sleep = 0.01):
     pyautogui.click(pos[0], pos[1])
-    time.sleep(x)
+    if colorPos:
+        while ImageGrab.grab().getpixel(colorPos) != color:
+            pass
+    time.sleep(sleep)
 
 # Connect to Pulse
-click(wifi, 1)
-click(pulse, 1)
-click(pulse2)
-click(connect, 1)
+click(wifi, (1058, 173), (255, 255, 255))
+click(pulse, (352, 272), (0, 118, 215))
+click(pulse2, (512, 318), (194, 194, 194))
+click(connect, (413, 659), (48, 48, 48))
 click(proceed)
 click(next)
-click(next2, 0.15)
+click(next2)
+click(password)
 click(password)
 pyautogui.write('push')
 pyautogui.press('enter')
--- position.py
+++ position.py
@@ -1,6 +1,10 @@
 import pyautogui
+from PIL import ImageGrab
+import time
 
 while True:
     x, y = pyautogui.position()
-    if pyautogui.confirm(text=str(x) + ' ' + str(y), title='Position', buttons=['OK', 'Cancel']) == 'Cancel':
-        break
+    print(x, y, ImageGrab.grab().getpixel((x, y)))
+    time.sleep(1)
+#    if pyautogui.confirm(text=str(x) + ' ' + str(y), title='Position', buttons=['OK', 'Cancel']) == 'Cancel':
+#        break
